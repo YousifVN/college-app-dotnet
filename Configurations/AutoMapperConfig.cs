247aa1a81e89babc0ff78d1d5b135d8d522cd5d1
YousifVN <yousifmahmood995@gmail.com>
2025-06-18 03:49:43 +0300

    Auto mapper with different property names, ignore, transform

--- Configurations/AutoMapperConfig.cs
+++ Configurations/AutoMapperConfig.cs
@@ -8,6 +8,21 @@ public class AutoMapperConfig : Profile
 {
     public AutoMapperConfig()
     {
+        // CreateMap<Student, StudentDTO>();
+        // CreateMap<StudentDTO, Student>();
+        
+        // we can use this instead of the two lines above
         CreateMap<StudentDTO, Student>().ReverseMap();
+        
+        // ** make sure to add future configs after the reverse to avoid unexpected behaviors **
+        
+        // config for different property names
+        // CreateMap<StudentDTO, Student>().ReverseMap().ForMember(n => n.Name, opt => opt.MapFrom(x => x.StudnetName));
+        
+        // config for ignore
+        // CreateMap<StudentDTO, Student>().ReverseMap().ForMember(n => n.Email, opt => opt.Ignore());
+        
+        // config for transform
+        // CreateMap<StudentDTO, Student>().ReverseMap().AddTransform<string>(n => string.IsNullOrEmpty(n) ? "No address found" : n);
     }
 }
