ec9dacb95e4c93bc6346cbdd4ddb5dba2febd06e
YousifVN <yousifmahmood995@gmail.com>
2025-06-28 00:14:37 +0300

    create RoleDTO

---
+++ Models/RoleDTO.cs
@@ -0,0 +1,16 @@
+using Microsoft.Build.Framework;
+
+namespace CollegeApp.Models;
+
+public class RoleDTO
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public string RoleName { get; set; }
+    
+    public string Description { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
+}
